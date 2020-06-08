d66b9c375683a228f4bc4a08d91b52728c6c48ac
kolyamba23 <60772584+kolyamba23@users.noreply.github.com>
2020-06-08 20:59:21 +0300

    Kontsevoy_Lab_01_Var_3

---
+++ lab_01/src/Queue.h
@@ -0,0 +1,221 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Queue
+{
+private:
+	
+	unsigned int count { 0 };
+	unsigned int size { 1 };
+	int* buffer;
+public:
+	Queue();
+	~Queue();
+	void push(int t1);
+	int pop();
+	int peek();
+	int countelement();
+	void Delete_Negative_Element();
+	void Delete_IN_Diapazon(int start, int end);
+	void vyvodelementa(int index);
+	bool operator == (const Queue & struct2)
+	{ 
+		if (count == struct2.count)
+		{
+			int i = 0;
+			do
+			{
+				if (buffer[i] != struct2.buffer[i]) return false;
+ i++;
+ } while ((buffer[i] == struct2.buffer[i]) && (i < count));
+		return true;
+		};
+		return false;
+	};
+
+	bool operator !=(const Queue& struct2)
+	{
+		return !(*this == struct2);
+	};
+
+	bool operator > (const Queue& struct2)
+	{
+		int i = 0;
+		if (count >= struct2.count)
+		{
+			do
+			{
+				if (buffer[i] < struct2.buffer[i]) return false;
+				else if (buffer[i] > struct2.buffer[i]) return true;
+				i++;
+			} while ((buffer[i] == struct2.buffer[i]) && (i < struct2.count));
+			if (count > struct2.count) return true;
+			else return false;
+		}
+		else
+		{
+			do
+			{
+				if (buffer[i] < struct2.buffer[i]) return false;
+				else if (buffer[i] > struct2.buffer[i]) return true;
+				i++;
+			} while ((buffer[i] == struct2.buffer[i]) && (i < struct2.count));
+		}
+		return false;
+	};
+
+	bool operator < (const Queue& struct2)
+	{
+
+		int i = 0;
+		if (count >= struct2.count)
+		{
+			do
+			{
+				if (buffer[i] < struct2.buffer[i]) return true;
+				else if (buffer[i] > struct2.buffer[i]) return false;
+				i++;
+			} while ((buffer[i] == struct2.buffer[i]) && (i < struct2.count));
+			if (count > struct2.count) return false;
+			else return true;
+		}
+		else
+		{
+			do
+			{
+				if (buffer[i] < struct2.buffer[i]) return true;
+				else if (buffer[i] > struct2.buffer[i]) return false;
+				i++;
+			} while ((buffer[i] == struct2.buffer[i]) && (i < count));
+		}
+		return true;
+	};
+
+	bool operator <= (const Queue& struct2)
+	{
+		return !(*this > struct2);
+	};
+
+	bool operator >= (const Queue& struct2)
+	{
+		return !(*this < struct2);
+	};
+};
+
+Queue::Queue()
+{};
+
+Queue::~Queue()
+{
+	delete[] buffer;
+};
+
+void Queue::vyvodelementa(int index)
+{
+	if (index > (count - 1))//If we want to call an element that doesn't exist
+		throw std::range_error("No this element");
+	cout << buffer[index] << " ";
+}
+
+void Queue::push(int t1)
+{
+	if ((count + 1) > size)
+	{
+		if (count == 0)
+			buffer = new int[size];
+		int* Mass;
+		size = size + 5;
+		Mass = new int[size];
+		for (int i = 0; i < size; i++)
+			Mass[i] = buffer[i];
+		Mass[count] = t1;
+		delete[]buffer;
+		buffer = Mass;
+		count++;
+	}
+	else {
+		if (count == 0)
+			buffer = new int[size];
+		buffer[count] = t1;
+		count++;
+	}
+};
+
+int Queue::pop()
+{
+	if (count < 1)
+		throw std::range_error("Leaving the queue");
+	int ourelement = buffer[0];
+	size--;//No cleanup of memory used
+	int* Mass;
+	Mass = new int[size];
+	for (int i = 0; i < size; i++)
+		Mass[i] = buffer[i + 1];
+	delete[]buffer;
+	buffer = Mass;
+	count--;
+	return ourelement;
+};
+
+int Queue::peek()
+{
+	if (count < 1)
+		throw std::range_error("Leaving the queue");
+	return buffer[0];
+};
+
+int Queue::countelement()
+{
+	return count;
+}
+
+void Queue::Delete_Negative_Element()
+{
+	for (int i = 0; i < count; i++)
+	{
+		if (buffer[i] < 0)
+		{
+			if (i == count)
+			{
+				count--;
+			}//Preventing out-of-bounds
+			else {
+				for (int j = i; j < count; j++)
+				{
+					buffer[j] = buffer[j + 1];
+				}
+			}
+			count--;
+			i--;
+		}
+	}
+
+	for (int i = 0; i < count; i++)
+		cout << buffer[i] << " ";
+};
+
+void Queue::Delete_IN_Diapazon(int start, int end)
+{
+	for (int i = 0; i < count; i++)
+	{
+		if ((buffer[i] >= start) && (buffer[i] <= end))
+		{
+			if (i == count)
+			{
+				count--;
+			}//Preventing out-of-bounds
+			else {
+				for (int j = i; j < count; j++)
+				{
+					buffer[j] = buffer[j + 1];
+				}
+			}
+			count--;
+			i--;
+		}
+	}
+
+	for (int i = 0; i < count; i++)
+		cout << buffer[i] << " ";
+};
---
+++ lab_01/src/Stack.h
@@ -0,0 +1,228 @@
+# pragma once
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+private:
+	unsigned int count { 0 };
+	unsigned int size { 1 };
+	
+public:
+	Stack();
+	~Stack();
+	int* buffer;
+	void push(int t1);
+	int pop();
+	int peek();
+	int countelement();
+	void Delete_Negative_Element();
+	void Delete_IN_Diapazon(int start, int end);
+	void vyvodelementa(int index);
+	bool operator == (const Stack & struct2)
+	{
+		if (count == struct2.count)
+		{
+			int i = count;
+			do
+			{
+				if (buffer[i] != struct2.buffer[i]) return false;
+				i--;
+			} while ((buffer[i] == struct2.buffer[i]) && (i != 0));
+			return true;
+		}
+		else return false;
+	};
+
+	bool operator !=(const Stack& struct2)
+	{
+		return !(*this == struct2);
+	};
+
+	bool operator > (const Stack& struct2)
+	{
+		int i = count-1;
+		int j = struct2.count-1;
+		if (count >= struct2.count)
+		{	
+			do
+			{
+				if (buffer[i] < struct2.buffer[j]) return false;
+				else if (buffer[i] > struct2.buffer[j]) return true;
+				i--;
+				j--;
+			} while ((buffer[i] == struct2.buffer[j]) && (j != 0));
+			if (count > struct2.count) return true;
+			else return false;
+		}
+		else
+		{
+			do
+			{
+				if (buffer[i] < struct2.buffer[j]) return false;
+				else if (buffer[i] > struct2.buffer[j]) return true;
+				i--;
+				j--;
+			} while ((buffer[i] == struct2.buffer[j]) && (i != 0));
+			return false;
+		}
+		
+	};
+
+	bool operator < (const Stack& struct2)
+	{
+		int i = count-1;
+		int j = struct2.count-1;
+		if (count >= struct2.count)
+		{
+			
+			do
+			{
+				if (buffer[i] < struct2.buffer[j]) return true;
+				else if (buffer[i] > struct2.buffer[i]) return false;
+				i--;
+				j--;
+			} while ((buffer[i] == struct2.buffer[i]) && (i != 0));
+			if (count > struct2.count) return false;
+			else return true;
+		}
+		else
+		{
+			do
+			{
+				if (buffer[i] < struct2.buffer[j]) return true;
+				else if (buffer[i] > struct2.buffer[j]) return false;
+				i--;
+				j--;
+			} while ((buffer[i] == struct2.buffer[j]) && (j != 0));
+			return true;
+		}
+		
+	};
+
+	bool operator <= ( const Stack& struct2)
+	{
+		return !(*this > struct2);
+	};
+
+	bool operator >= (const Stack& struct2)
+	{
+		return !(*this < struct2);
+	};
+};
+
+Stack::Stack()
+{};
+
+Stack::~Stack()
+{delete[] buffer;};
+
+void Stack::vyvodelementa(int index)
+{
+	if (index > (count - 1))//If we want to call an element that doesn't exist
+		throw std::range_error("No this element");
+	cout << buffer[index] << " ";
+}
+
+void Stack::push(int t1)
+{
+	if ((count + 1) > size)
+	{
+		if (count == 0)
+			buffer = new int[size];
+		int* Mass;
+		size = size + 5;
+		Mass = new int[size];
+		for (int i = 0; i < size; i++)
+			Mass[i] = buffer[i];
+		Mass[count] = t1;
+		delete[]buffer;
+		buffer = Mass;
+		count++;
+	}
+	else {
+		if (count == 0)
+			buffer = new int[size];
+		buffer[count] = t1;
+		count++;
+	}
+};
+
+int Stack::pop()
+{
+	if (count < 1)
+		throw std::range_error("Going beyond the vector");
+	int vozvrat = buffer[count - 1];
+	size--;//No cleanup of memory used
+	int* Mass;
+	Mass = new int[size];
+	for (int i = 0; i < size; i++)
+		Mass[i] = buffer[i];
+	delete[]buffer;
+	buffer = Mass;
+	count--;
+	return vozvrat;
+};
+
+int Stack::peek()
+{
+	return buffer[count - 1];
+};
+
+int Stack::countelement()
+{
+
+	if (count < 1)
+		throw std::range_error("Going off the stack");
+	return count;
+};
+
+void Stack::Delete_Negative_Element()
+{
+	for (int i = 0; i < count; i++)
+	{
+		if (buffer[i] < 0)
+		{
+			if (i == count)
+			{
+				count--;
+			}//Preventing out-of-bounds
+			else {
+				for (int j = i; j < count; j++)
+				{
+					buffer[j] = buffer[j + 1];
+				}
+			}
+			count--;
+			i--;
+		}
+	}
+
+	for (int i = 0; i < count; i++)
+		cout << buffer[i] << " ";
+};
+
+void Stack::Delete_IN_Diapazon(int start, int end)
+{
+	for (int i = 0; i < count; i++)
+	{
+		if ((buffer[i] >= start) && (buffer[i] <= end))
+		{
+			if (i == count)
+			{
+				count--;
+			}//Preventing out-of-bounds
+			else {
+				for (int j = i; j < count; j++)
+				{
+					buffer[j] = buffer[j + 1];
+				}
+			}
+			count--;
+			i--;
+		}
+	}
+
+	for (int i = 0; i < count; i++)
+		cout << buffer[i] << " ";
+};
